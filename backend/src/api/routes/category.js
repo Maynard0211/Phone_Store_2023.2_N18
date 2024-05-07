4d6fa416c34282a822bdf3e2d27fa9582a6ec169
Duckkk <trongduc2003ht@gmail.com>
2024-05-07 19:36:48 +0700

    Category APIs

---
+++ backend/src/api/routes/category.js
@@ -0,0 +1,43 @@
+import express, { query, response } from 'express';
+import responseError from '../res/response.js';
+import { callRes } from '../res/response.js';
+
+// Import database connection
+import connection from '../../db/connect.js';
+
+const router = express.Router();
+
+router.post('/add', (req, res, next) => {
+    var category = req.body; 
+    var query = "INSERT INTO category (name) values (?)";
+    console.log(category);
+    connection.query(query,[category.name], (err, results) => {
+      console.log(11, results);
+      if (!results) return callRes(res, responseError.UNKNOWN_ERROR, null);
+      return callRes(res, responseError.OK);
+    });
+  });
+  
+router.get('/get', async (req,res)=>{
+    var query=`select * from category order by name`;
+    connection.query(query, (err, results) => {
+        if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
+        return callRes(res, responseError.OK, results);
+    });
+});
+
+router.patch('/update',(req,res)=>{
+    let product =req.body
+    var query=`update category set name=? where id=?`;
+    connection.query(query,[product.name,product.id], (err, results) => {
+        if (!err){
+            if(results.affectedRows==0){
+                return callRes(res, responseError.OK, results);
+            }
+            return callRes(res, responseError.OK, results);
+        } 
+        else {return callRes(res, responseError.UNKNOWN_ERROR, null);}
+    });
+})
+
+export { router };
--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,9 +1,11 @@
 import { router as productRouter } from "./product.js";
 import { router as orderRouter } from "./order.js";
 import { router as userRouter } from "./users.js";
+import { router as categoryRouter } from "./category.js";
 
 function route(app) {
     app.use('/product', productRouter);
+    app.use('/category', categoryRouter)
     app.use('/order', orderRouter);
     app.use('/use', userRouter);
 }
