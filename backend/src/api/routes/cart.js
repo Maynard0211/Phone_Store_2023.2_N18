954a316c2bbf63a6ca6f5e0bfacaf15f35f508ce
tiendung <ntnt081102@gmail.com>
2024-05-12 22:36:41 +0700

    modified:   backend/src/api/routes/cart.js

--- backend/src/api/routes/cart.js
+++ backend/src/api/routes/cart.js
@@ -1,4 +1,5 @@
 import express, { query } from "express";
+import jsonwebtoken from 'jsonwebtoken';
 import responseError from "../res/response.js";
 import { callRes } from "../res/response.js";
 
@@ -8,35 +9,33 @@ import connection from "../../db/connect.js";
 
 const router = express.Router();
 
-const JWT_SECRET = 'secret_code';
+const JWT_SECRET = 'maBiMat';
 
 // Thêm sản phẩm vào cart của user
 router.post("/addToCart", (req, res) => {
-    const { token, productID, quantity } = req.body;
-
+    const { token} = req.body;
+    
     if (!token) return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH, null);
     // Giải mã token, lấy ra userId
-    try {
-      const decoded = jsonwebtoken.verify(token, JWT_SECRET);
-      const userID = decoded.userId;
-      }
-      catch (error) {
-        return callRes(res, responseError.TOKEN_IS_INVALID,null);
-    }
+    
+    const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+    const userId = decoded.userId;
+    const productId = decoded.productId;
+    const quantity = decoded.quantity;
     
 
     // Kiểm tra tính hợp lệ của dữ liệu (tùy chọn)
-    if (!userID || !productID || !quantity) {
+    if (!userId || !productId || !quantity) {
       return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH,null);
     }
-  
+    
     // Tạo câu truy vấn SQL
     const query = `INSERT INTO cart (userId, productId, quantity) VALUES (?, ?, ?)`;
   
     // Thực hiện truy vấn
     connection.query(
       query,
-      [userID, productID, quantity],
+      [userId, productId, quantity],
       (err, results) => {
         console.log(err);
         if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
@@ -52,21 +51,16 @@ router.get("/get", async(req, res) => {
   // Kiểm tra xem token có tồn tại không
   if (!token) return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH, null);
   // Giải mã token, lấy ra userId  
-  try {
-    const decoded = jsonwebtoken.verify(token, JWT_SECRET);
-    const userID = decoded.userId;
-    }
-    catch (error) {
-      return callRes(res, responseError.TOKEN_IS_INVALID,null);
-    }
   
-  const query = ` SELECT FROM cart WHERE userId = ? `;
-  if (!userID) {
+  const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+  const userId = decoded.userId;
+  const query = `SELECT name FROM product INNER JOIN users as u WHERE u.id = ${userId} `;
+  if (!userId) {
     return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH,null);
   }
   connection.query(
     query,
-    [userID],
+    [userId],
     (err, results) => {
       console.log(err);
       if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
@@ -78,80 +72,69 @@ router.get("/get", async(req, res) => {
 
 // Xóa sản phẩm khỏi cart của user
 router.delete("/deleteFromCart", async (req, res) => {
-  const { token, productID } = req.body;
+  const {token} = req.body;
+  
   // Kiểm tra xem token có tồn tại không
   if (!token) return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH, null);
   // Giải mã token, lấy ra userId  
-  try {
+  
   const decoded = jsonwebtoken.verify(token, JWT_SECRET);
-  const userID = decoded.userId;
-  }
-  catch (error) {
-    return callRes(res, responseError.TOKEN_IS_INVALID,null);
-  }
+  const userId = decoded.userId;
+  const productId = decoded.productId;
   // Kiểm tra tính hợp lệ của dữ liệu
-  if (!userID || !productID) {
+  if (!userId || !productId) {
     return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH,null);
   }
-
+  
   // Tạo câu truy vấn SQL
   const query = ` DELETE FROM cart WHERE userId = ? AND productId = ? `;
-
-  // Thực hiện truy vấn
-  try {
-    const [results] = connection.query(query, [userID, productID]);
-
-    // Kiểm tra số lượng hàng bị ảnh hưởng
-    if (results.affectedRows === 0) {
-      return callRes(res, responseError.NO_DATA_OR_END_OF_LIST_DATA,null);
+  if (!userId) {
+    return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH,null);
+  }
+  connection.query(
+    query,
+    [userId,productId],
+    (err, results) => {
+      console.log(err);
+      if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
+      return callRes(res, responseError.OK, results);
     }
-
-    // Trả về kết quả thành công
-    return callRes(res, responseError.OK, results);
-  } catch (error) {
-    // Xử lý lỗi
-    return callRes(res, responseError.UNKNOWN_ERROR, null);
-  } 
+  );
 });
 
 
 // Cập nhật số lượng sản phẩm vào cart của user
 router.patch("/updateCart", async (req, res) => {
-  const { token, productID, quantity } = req.body;
-
+  const {token} = req.body;
+  
+  // Kiểm tra xem token có tồn tại không
   if (!token) return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH, null);
   // Giải mã token, lấy ra userId  
-  try {
-    const decoded = jsonwebtoken.verify(token, JWT_SECRET);
-    const userID = decoded.userId;
-    }
-    catch (error) {
-      return callRes(res, responseError.TOKEN_IS_INVALID,null);
-    }
+  
+  const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+  const userId = decoded.userId;
+  const productId = decoded.productId;
+  const quantity = decoded.quantity;
   // Kiểm tra tính hợp lệ của dữ liệu
-  if (!userID || !productID || !quantity) {
+  if (!userId || !productId || !quantity) {
     return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH,null);
   }
-
-
+  
   // Tạo câu truy vấn SQL
-  const query = ` UPDATE cart SET quantity = ? WHERE userId = ? AND productId = ? `;
-
-  // Thực hiện truy vấn
-  try {
-    const [results] = connection.query(query, [quantity, userID, productID]);
-
-    // Kiểm tra số lượng hàng bị ảnh hưởng
-    if (results.affectedRows === 0) {
-      return callRes(res, responseError.NO_DATA_OR_END_OF_LIST_DATA,null);
+  const query = ` UPDATE cart SET quantity = '${quantity}' WHERE userId = '${userId}' AND productId = '${productId}'`;
+  if (!userId) {
+    return callRes(res, responseError.PARAMETER_IS_NOT_ENOUGH,null);
+  }
+  connection.query(
+    query,
+    [userId,productId,quantity],
+    (err, results) => {
+      console.log(err);
+      if (err) return callRes(res, responseError.UNKNOWN_ERROR, null);
+      return callRes(res, responseError.OK, results);
     }
-
-    // Trả về kết quả thành công
-    return callRes(res, responseError.OK, results);
-  } catch (error) {
-    // Xử lý lỗi
-    return callRes(res, responseError.UNKNOWN_ERROR, null);
-  } 
+  );
+  
 });
 
 
