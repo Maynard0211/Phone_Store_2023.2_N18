708c6be2333168d4280954b16403c57355b3ba1f
Lenhatduc <lenhatduc30042003@gmail.com>
2024-05-16 15:18:10 +0700

    update 7: them mot so  anh va chinh lai phan bieu do

--- admin/src/components/Layout/Layout.jsx
+++ admin/src/components/Layout/Layout.jsx
@@ -10,7 +10,14 @@ import PropTypes from "prop-types";
 import Sidebar from "../Sidebar/Sidebar";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import Dashboard from "../../pages/dashboard/DashBoard";
+import Breadcrumbs from "../Breadbrumbs/Breadcrumbs";
+import Dashboard from "../../pages/dashboard/Dashboard";
+import Typography from "../../pages/typography/Typography";
+import Notifications from "../../pages/notifications/Notifications";
+import Tables from "../../pages/tables/Tables";
+import Charts from "../../pages/uielements/charts/Charts";
+import Icons from "../../pages/uielements/icons/IconsPage";
+import Maps from "../../pages/uielements/maps/google/GoogleMapPage";
 
 // -- Component Styles
 import s from "./Layout.module.scss";
@@ -34,19 +41,27 @@ const Layout = (props) => {
         <Sidebar />
         <main className={s.content}>
           <Switch>
-            <Route path="/template" exact render={() => <Redirect to="template/dashboard"/>} />
-            <Route path="/template/dashboard" exact component={Dashboard}/> 
+          <Route path="/template" exact render={() => <Redirect to="template/dashboard"/>} />
+            <Route path="/template/dashboard" exact component={Dashboard}/>
+            <Route path="/template/typography" exact component={Typography} />
+            <Route path="/template/tables" exact component={Tables} />
             <Route path="/template/products" exact component={Products} />
-            <Route path="/template/editProduct/:slug" component={EditProduct} />
             <Route path="/template/insertProduct" component={InsertProduct} />
+            <Route path="/template/editProduct/:slug" component={EditProduct} />
             <Route path="/template/createPhoneSale" exact component={CreatePhoneSale} />
-            <Route path="/template/editPhoneSale/:slug" exact component={EditPhoneSale} />
             <Route path="/template/viewsPhoneSale" exact component={ViewsPhoneSale} />
+            <Route path="/template/editPhoneSale/:slug" exact component={EditPhoneSale} />
             <Route path="/template/createEnterPhone" exact component={CreateEnterPhone} />
-            <Route path="/template/editEnterPhone/:slug" exact component={EditEnterPhone} />
             <Route path="/template/viewsEnterPhone" exact component={ViewsEnterPhone} />
+            <Route path="/template/editEnterPhone/:slug" exact component={EditEnterPhone} />
             <Route path="/template/chart" exact component={Chart} />
             <Route path="/template/orders" exact component={Orders} />
+            <Route path="/template/notifications" exact component={Notifications} />
+            <Route path="/template/ui-elements" exact render={() => <Redirect to={"/template/ui-elements/charts"} />} />
+            <Route path="/template/ui-elements/charts" exact component={Charts} />
+            <Route path="/template/ui-elements/icons" exact component={Icons} />
+            <Route path="/template/ui-elements/maps" exact component={Maps} />
+            <Route path='*' exact render={() => <Redirect to="/error" />} />
           </Switch>
         </main>
         <Footer />
